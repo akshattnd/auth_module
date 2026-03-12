60d528423d84ee709cacab47b763d0b13c95b864
akshattnd <akshattandon940@gmail.com>
2026-03-12 09:54:38 +0000

    added decode_access_token and oauth method

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,10 @@
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta, UTC
-
+from fastapi.security import OAuth2PasswordBearer
 SECRET_KEY = "supersecret"
 ALGORITHM = "HS256"
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password:str)-> str:
@@ -18,3 +18,10 @@ def create_access_token(data:dict):
     expire = datetime.now(UTC) + timedelta(hours=2)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token:str):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
